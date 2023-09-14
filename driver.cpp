5b1c7ac98371b772a26a615854ae4cdfe8200f09
Ayush Tambde <tambdeayush@gmail.com>
2023-09-14 19:20:31 +0100

    got SRAM copying code working... now for tiled matmul

--- driver.cpp
+++ driver.cpp
@@ -9,29 +9,77 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <math.h>
 
-int main() {
-	MTL::Device* dev = MTL::CreateSystemDefaultDevice();
-	//std::cout << dev->maxThreadgroupMemoryLength() << "\n";
-	MTL::CommandQueue* cmd_queue = dev->newCommandQueue();
+std::string ReadMetalFile() {
 	
-	int mat_size = 5;
-	int total_el_size = mat_size * mat_size;
-	
-	float buffer_cpu[total_el_size]; 
-
-	for(int i = 0; i < total_el_size; i++) {
-		buffer_cpu[i] = (i);
-	}
-
         std::ifstream shader_file("flash.metal");
         std::stringstream text_buffer;
         text_buffer << shader_file.rdbuf();
 	
 	std::string temp_str = text_buffer.str();
-        const char* str = temp_str.c_str();
+        return temp_str; 
 	
 
+}
+
+
+void create_prod_arr(int* shape_arr, int arr_size, int* output_arr, int curr_index) {
+	if(curr_index == arr_size-1) {
+		output_arr[arr_size - 1] = shape_arr[arr_size-1];	
+	}
+
+	else {
+		output_arr[curr_index] = output_arr[curr_index+1] * shape_arr[curr_index];
+	}	
+
+	if(curr_index == 0) return;
+
+	create_prod_arr(shape_arr, arr_size, output_arr, curr_index-1);
+}
+
+
+
+
+void print_tensor(float* buff, int* shape, unsigned int num_dim) {
+	// strategy: print in reverse dimensions
+	
+	int prod_arr[num_dim];
+	create_prod_arr(shape, num_dim, prod_arr, num_dim-1);
+
+	int total_elements = prod_arr[0];
+
+	for(int j = 0; j < total_elements; j++) { 
+		printf("%f ", buff[j]); 
+		for(int i = 0; i < num_dim; i++) {
+			if((j+1) % prod_arr[i] == 0) {
+				printf("\n");
+			}
+
+		}
+	}
+
+
+}
+
+
+int main() {
+	
+	// create metal device
+	MTL::Device* dev = MTL::CreateSystemDefaultDevice();
+	
+	// print out GPU metadata
+	std::cout << "MAX THREADGROUP MEMORY: " << dev->maxThreadgroupMemoryLength() << "\n";
+//	std::cout << dev->recommendedMaxWorkingSetSize() << "\n";
+	// create command queue where we will dispatch our jobs
+	MTL::CommandQueue* cmd_queue = dev->newCommandQueue();
+
+	std::string temp_file = ReadMetalFile();
+	const char* str = temp_file.c_str();
+
+//	std::cout << str << "\n";
+	
+	// copy-pasted error-handling code, too bored to write it :)
 	NS::Error* err = nullptr;
 	MTL::Library* library= dev->newLibrary(NS::String::string(str, NS::UTF8StringEncoding), nullptr, &err);//newLibrary(NS::String::string("flash", NS::UTF8StringEncoding), nullptr, &err);
 
@@ -42,24 +90,70 @@ int main() {
 	}
 
 
+/*
+
+	unsigned int n_embed = 6;
+	unsigned int N_seq = 6;
+	int total_el_size = N_seq * n_embed; 
+
+	// load function from metal shader file
 	MTL::Function* kernelFunc = library->newFunction(NS::String::string("mat_mul", NS::UTF8StringEncoding));
 	MTL::ComputePipelineState* pipeline= dev->newComputePipelineState(kernelFunc, &err);
-	MTL::Buffer* buff1 = dev->newBuffer(total_el_size, MTL::ResourceStorageModeShared);
-	MTL::Buffer* buff2 = dev->newBuffer(total_el_size, MTL::ResourceStorageModeShared);
+
+
+	// attention buffers, ie Q,K,V, shape = (N_seq, n_embed)
+	MTL::Buffer* query= dev->newBuffer(total_el_size, MTL::ResourceStorageModeShared);
+	MTL::Buffer* key= dev->newBuffer(total_el_size, MTL::ResourceStorageModeShared);
+	MTL::Buffer* value= dev->newBuffer(total_el_size, MTL::ResourceStorageModeShared);
+	
+	// ancillary buffers, shape = (N_seq)
+	MTL::Buffer* l_vals =  dev->newBuffer(N_seq, MTL::ResourceStorageModeShared);
+	MTL::Buffer* m_vals =  dev->newBuffer(N_seq, MTL::ResourceStorageModeShared); 
+
+	// Output, shape = (N_seq, n_embed)	
 	MTL::Buffer* buff_out = dev->newBuffer(total_el_size, MTL::ResourceStorageModeShared);
 	
+
+
+	
+	// copying data into CPU buffer
+	float buffer_cpu[total_el_size]; 
+
+	for(int i = 0; i < total_el_size; i++) {
+		buffer_cpu[i] = i;
+		(float*)(buff_out->contents())[i] = 2.5f;
+	}
+	
+
+	// copying CPU buffers into HBM memory buffer objects
+	memcpy(query->contents(), buffer_cpu, total_el_size * sizeof(float));
+	memcpy(key->contents(), buffer_cpu, total_el_size * sizeof(float));
+	memcpy(value->contents(), buffer_cpu, total_el_size * sizeof(float));
+
+	// copying -inf to m_vals and 0 to l_vals:
+	for(int i = 0; i < N_seq; i++) {
+		(float*)(l_vals->contents())[i] = 0;
+		(float*)(m_vals->contents())[i] = -INFINITY;
+
+	}	
+
+
 	MTL::CommandQueue* commandQueue = dev->newCommandQueue();
 	MTL::CommandBuffer* commandBuffer = commandQueue->commandBuffer();
 	MTL::ComputeCommandEncoder* encoder = commandBuffer->computeCommandEncoder();
+	
+
 	encoder->setComputePipelineState(pipeline);
 
-	memcpy(buff1->contents(), buffer_cpu, total_el_size * sizeof(float));
-	memcpy(buff2->contents(), buffer_cpu, total_el_size * sizeof(float));
 
-	encoder->setBuffer(buff1, 0, 0);
-	encoder->setBuffer(buff2, 0, 1);
-	encoder->setBuffer(buff_out, 0, 2);
+	encoder->setBuffer(query, 0, 0);
+	encoder->setBuffer(key, 0, 1);
+	encoder->setBuffer(value, 0, 2);
+	encoder->setBuffer(buff_out, 0, 3);
+	encoder->setBuffer(l_vals, 0, 4);
+	encoder->setBuffer(m_vals, 0, 5);
 
+	// setting threads and threadgroup sizes
 	MTL::Size threads_threadgroup; 
 	MTL::Size threadgroup_per_grid;
 
@@ -71,18 +165,23 @@ int main() {
 	threadgroup_per_grid.width  = 5;
 	threadgroup_per_grid.depth = 1;
 
-	
+	// perform computation	
+	// dispatch threads to GPU
 	encoder->dispatchThreadgroups(threadgroup_per_grid, threads_threadgroup);
 	encoder->endEncoding();
+	// commit jobs 
 	commandBuffer->commit();
+	// let CPU wait on GPU tasks
 	commandBuffer->waitUntilCompleted();
+
+	// print output contents (viz)
 	float* output_buffer = (float*)(buff_out->contents());
 	
-
+	// print out buffer values
 	for(int i = 0; i < total_el_size; i++) {
 		std::cout << output_buffer[i] << "\n";
 	}
-
+*/
 	dev->release();
 
 
--- flash.metal
+++ flash.metal
@@ -14,25 +14,50 @@ kernel void mat_mul(const device float* mat1 [[buffer(0)]], const device float* 
 
 
 
-kernel void attention(const device float* query[[buffer(0)]], const device float* key[[buffer(1)]], const device float* value[[buffer(2)]], device float* out [[buffer(3)]], uint2 gid [[thread_position_in_grid]], uint tid [[thread_position_in_threadgroup]]) {
+kernel void attention(const device float* query[[buffer(0)]], const device float* key[[buffer(1)]], const device float* value[[buffer(2)]], const device float* out [[buffer(3)]], uint2 gid [[thread_position_in_grid]], uint2 tid [[thread_position_in_threadgroup]]) {
         
-
-	int BLOCK_SIZE = 1;	
-	float total = 0;
-	int MATRIX_SIZE = 5;
-
-	threadgroup float SRAM[MATRIX_SIZE * MATRIX_SIZE]; 
-	for(int i = tid; i < 	
-	
-
 
         int i = gid.y;
         int j = gid.x;
         int ROW_SIZE = 5;
+	
+	int BLOCK_SIZE = 1;	
+	
+	unsigned int elements_to_copy = (ROW_SIZE / BLOCK_SIZE);
+	
+	// SRAM holds both key and query 
+	const unsigned int sram_len = 2 * ROW_SIZE * BLOCK_SIZE;
+	threadgroup float SRAM[400];
+	unsigned int offset_block = ROW_SIZE * BLOCK_SIZE;	
+	
+	
+	// initial partial transfer to SRAM
+	for(int k = 0; k < elements_to_copy; k++) {
+		unsigned int temp_ind = (tid.x * elements_to_copy) + k;
+		SRAM[temp_ind] = query[temp_ind];
+		SRAM[offset_block + temp_ind] = key[temp_ind];
+	}
+
+	threadgroup_barrier(mem_flags::mem_threadgroup);
+
+	// trying to do matmul now...
+	// so, basically I have to calculate a single dot product, with a single loop, for a pair of rows
+	// I have to find a particular row in SRAM: sram[i * row_size + j], where i ranges from 0 to block size, to isolate row in block. Then j just 
+	// gets us where we need to go locally within that row, so from 0-row_size
+	// And for key I just need to add offset_block
+	
+	float total = 0;
+	
+	for(int j = 0; j < ROW_SIZE; j++) {
+		total += SRAM[tid.x * BLOCK_SIZE + j] + SRAM[offset_block + (tid.y * BLOCK_SIZE + j)];	
+
+	}
+
+	/*
         for(int k = 0; k < ROW_SIZE; k++) {
                 total += (query[ROW_SIZE * k + i] * key[j* ROW_SIZE + k]);
         }
 		
         out[j*ROW_SIZE + i] = metal::exp(total); 
-
+*/
 }
